a43b4e1c4a9e2498b516474f1369a5c771d3c936
ccarlos19 <57632969+ccarlos19@users.noreply.github.com>
2019-11-19 18:35:22 -0500

    Ventanas

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -6,11 +6,11 @@ import Vista.VentanaPrincipal;
 public class Controlador {
 	
 	public MenuPrincipal ventana_menu;
-	
+	public VentanaPrincipal ventanaP;
 	public Controlador() {
 	
 		ventana_menu = new MenuPrincipal();
-		
+		ventanaP = new VentanaPrincipal(); 
 	}
 	
 }
