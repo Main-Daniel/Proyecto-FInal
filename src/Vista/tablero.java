c9a6fa58f76bbdaa49cd09bc80d26c4e4e8df5ae
Daniel Ortiz <57633119+Main-Daniel@users.noreply.github.com>
2019-11-19 20:53:07 -0500

    Reparacion

--- src/Vista/tablero.java
+++ src/Vista/tablero.java
@@ -3,34 +3,30 @@ package Vista;
 import javax.swing.JPanel;
 
 public class tablero extends JPanel {
-	
+
 	public int matriz[][];
 	public int columnas; 
 	public int filas;
 	public VentanaPrincipal ventana = new VentanaPrincipal();
-	
+
 	public tablero() {
-		
+
 		columnas = 20;
 		filas = 5;
 		matriz = new int[filas][columnas];
-		
+
 		setVisible(true); 
 		imprimir();
-	    setLayout(null);
-	  
-		
+		setLayout(null);
+
+
 	}
-	
+
 	public void imprimir() {
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[i].length; j++) {
 			}
-			}
-			System.out.println();
 		}
+		System.out.println();
 	}
-	
-	
-
 }
