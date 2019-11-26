b5cbcf6a1589167c114e21d60ae1812a3f26f3bc
ccarlos19 <57632969+ccarlos19@users.noreply.github.com>
2019-11-26 14:08:41 -0500

    j

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -3,21 +3,12 @@ package Controlador;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.jar.JarException;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
-import com.sun.media.jfxmedia.events.PlayerEvent;
-
 import Vista.Ventana_Juego;
 import Vista.MenuPrincipal;
-
 
 import Vista.Ventana_Imagen;
 
@@ -26,8 +17,7 @@ import Vista.Ventana_Instrucciones;
 import Vista.Ventana_Numero_Jugadores;
 
 
-
-public class Controlador implements ActionListener, MouseListener {
+public class Controlador implements ActionListener {
 
 	public MenuPrincipal ventanaMenu;
 	public Ventana_Instrucciones ventanainstru;
@@ -41,9 +31,13 @@ public class Controlador implements ActionListener, MouseListener {
 
 	public Controlador() {
 
-		ventanaMenu = new MenuPrincipal();
+		ventanaMenu = new MenuPrincipal();	
 		ventanaMenu.panel1.boton.addActionListener(this);
+
+
 }
+
+
 
 
 	public void actionPerformed(ActionEvent e) {
@@ -55,28 +49,30 @@ public class Controlador implements ActionListener, MouseListener {
 			ventanainstru.instru.botonsiguiente.addActionListener(this);
 
 		}
-
+		
 		if(e.getActionCommand().equals("SIGUIENTE")) {
-
+			
 			ventanainstru.dispose();
 			ventanaNjugadores = new Ventana_Numero_Jugadores();
 			ventanaNjugadores.panel1.b2.addActionListener(this);
 			ventanaNjugadores.panel1.b3.addActionListener(this);
 			ventanaNjugadores.panel1.b4.addActionListener(this);
-
+			
+		}
+		
 		if(e.getActionCommand().equals("2")) {
-
+			
 			ventanaNjugadores.panel2.setVisible(true);
 			ventanaNjugadores.panel2.etiqueta1.setVisible(true);
 			ventanaNjugadores.panel2.nombre1.setVisible(true);
 			ventanaNjugadores.panel2.etiqueta2.setVisible(true);
 			ventanaNjugadores.panel2.nombre2.setVisible(true);
 			ventanaNjugadores.panel2.b_aceptar.addActionListener(this);
-
+			
 		}
-
+		
 		if(e.getActionCommand().equals("3")) {
-
+			
 			ventanaNjugadores.panel2.setVisible(true);
 			ventanaNjugadores.panel2.etiqueta1.setVisible(true);
 			ventanaNjugadores.panel2.nombre1.setVisible(true);
@@ -85,11 +81,11 @@ public class Controlador implements ActionListener, MouseListener {
 			ventanaNjugadores.panel2.etiqueta3.setVisible(true);
 			ventanaNjugadores.panel2.nombre3.setVisible(true);
 			ventanaNjugadores.panel2.b_aceptar.addActionListener(this);
-
+			
 		}
 
 		if(e.getActionCommand().equals("4")) {
-
+			
 			ventanaNjugadores.panel2.setVisible(true);
 			ventanaNjugadores.panel2.etiqueta1.setVisible(true);
 			ventanaNjugadores.panel2.nombre1.setVisible(true);
@@ -100,63 +96,25 @@ public class Controlador implements ActionListener, MouseListener {
 			ventanaNjugadores.panel2.etiqueta4.setVisible(true);
 			ventanaNjugadores.panel2.nombre4.setVisible(true);
 			ventanaNjugadores.panel2.b_aceptar.addActionListener(this);
-
+			
 		}
-
+	
 		if(e.getActionCommand().equals("Aceptar")) {
-
-			ventanaNjugadores.dispose();
-			ventanaima = new Ventana_Imagen();
-
+			ventanaima = new Ventana_Imagen(); 
+			
 			ventanaima.panel2.etiquetaimagen.setVisible(true);
 			imagenfichas = new ImageIcon(getClass().getResource("/Modelo/Ficha - 1.png"));
 			iconofichas = new ImageIcon(imagenfichas.getImage().getScaledInstance(150,150,Image.SCALE_DEFAULT));
-			ventanaima.panel2.etiquetaimagen.setIcon(iconofichas);
-
+			ventanaima.panel2.etiquetaimagen.setIcon(iconofichas);	
+			
 			ventanaima.panel1.color1.addActionListener(this);
 			ventanaima.panel1.color2.addActionListener(this);
 			ventanaima.panel1.color3.addActionListener(this);
 			ventanaima.panel1.color4.addActionListener(this);
-			ventanaima.panel1.botonselec.addActionListener(this);
-
+			
 		}
-
-		if(e.getActionCommand().equals("Azul")) {
-
-			imagenfichas = new ImageIcon(getClass().getResource("/Modelo/Ficha - 1.png"));
-			iconofichas = new ImageIcon(imagenfichas.getImage().getScaledInstance(150,150,Image.SCALE_DEFAULT));
-			ventanaima.panel2.etiquetaimagen.setIcon(iconofichas);
-
-		}else if(e.getActionCommand().equals("Rojo")) {
-
-			imagenfichas = new ImageIcon(getClass().getResource("/Modelo/Ficha - 2.png"));
-			iconofichas = new ImageIcon(imagenfichas.getImage().getScaledInstance(150,150,Image.SCALE_DEFAULT));
-			ventanaima.panel2.etiquetaimagen.setIcon(iconofichas);
-
-
-		}else if(e.getActionCommand().equals("Verde")) {
-
-			imagenfichas = new ImageIcon(getClass().getResource("/Modelo/Ficha - 3.png"));
-			iconofichas = new ImageIcon(imagenfichas.getImage().getScaledInstance(150,150,Image.SCALE_DEFAULT));
-			ventanaima.panel2.etiquetaimagen.setIcon(iconofichas);
-
-		}else if(e.getActionCommand().equals("Amarillo")) {
-
-			imagenfichas = new ImageIcon(getClass().getResource("/Modelo/Ficha - 4.png"));
-			iconofichas = new ImageIcon(imagenfichas.getImage().getScaledInstance(150,150,Image.SCALE_DEFAULT));
-			ventanaima.panel2.etiquetaimagen.setIcon(iconofichas);
-			ventanaima.panel1.botonselec.addActionListener(this);
-
-		}else if(e.getActionCommand().contentEquals("Seleccionar")) {
-
-			ventanajuego = new Ventana_Juego();
-			ventanajuego.musica.breproducir.addMouseListener(this);
-
-		}
-		}
-	}
-
 		
+<<<<<<< HEAD
 /*	public void Musica() {
 
 		try {
@@ -175,26 +133,46 @@ public class Controlador implements ActionListener, MouseListener {
 		} catch (JarException e1) {
 
 			e1.printStackTrace();
+=======
+			 if(e.getActionCommand().equals("Azul")) {
+				 
+					imagenfichas = new ImageIcon(getClass().getResource("/Modelo/Ficha - 1.png"));
+					iconofichas = new ImageIcon(imagenfichas.getImage().getScaledInstance(150,150,Image.SCALE_DEFAULT));
+					ventanaima.panel2.etiquetaimagen.setIcon(iconofichas);
+					
+			 }else if(e.getActionCommand().equals("Rojo")) {
+
+					imagenfichas = new ImageIcon(getClass().getResource("/Modelo/Ficha - 2.png"));
+					iconofichas = new ImageIcon(imagenfichas.getImage().getScaledInstance(150,150,Image.SCALE_DEFAULT));
+					ventanaima.panel2.etiquetaimagen.setIcon(iconofichas);
+					
+					
+			 }else if(e.getActionCommand().equals("Verde")) {
+
+					imagenfichas = new ImageIcon(getClass().getResource("/Modelo/Ficha - 3.png"));
+					iconofichas = new ImageIcon(imagenfichas.getImage().getScaledInstance(150,150,Image.SCALE_DEFAULT));
+					ventanaima.panel2.etiquetaimagen.setIcon(iconofichas);
+					
+			 }else if(e.getActionCommand().equals("Amarillo")) {
+
+					imagenfichas = new ImageIcon(getClass().getResource("/Modelo/Ficha - 4.png"));
+					iconofichas = new ImageIcon(imagenfichas.getImage().getScaledInstance(150,150,Image.SCALE_DEFAULT));
+					ventanaima.panel2.etiquetaimagen.setIcon(iconofichas);
+					
+					
+					
+					
+			 }
+				
+			}
+	
+>>>>>>> parent of c8522f4... tres
 			
 		}
-
-	}
-
-
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		
-	}
+	
 
-
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
+<<<<<<< HEAD
 	@Override
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
@@ -216,4 +194,6 @@ public class Controlador implements ActionListener, MouseListener {
 	}
 */
 }
+=======
+>>>>>>> parent of c8522f4... tres
 
